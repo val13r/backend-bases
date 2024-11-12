fe3fb8861467b53d3d688c9ae30a32b92d5fa893
Valentina <valenmr1513@gmail.com>
2024-11-12 00:03:46 -0500

    cors

--- app.js
+++ app.js
@@ -3,9 +3,9 @@ const userRoutes = require('./routes/userRoutes');
 const recursoRoutes = require('./routes/recursoRoutes');
 const categoriaAreaRoutes = require('./routes/categoriaAreaRoutes');
 const medioFormatoRoutes = require('./routes/medioFormatoRoutes');
-
+const cors = require("cors");
 const app = express();
-
+app.use(cors())
 app.use(express.json());
 
 
